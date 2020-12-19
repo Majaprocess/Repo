32435703825b7fe38d15b7d7c244be7d8138144f
Ruzica Ostojic <ruzicaostojic1992@gmail.com>
2020-12-19 15:44:47 +0100

    Izmena na master grani

--- GitProbaConsoleApp2/Program.cs
+++ GitProbaConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace GitProbaConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("proba2");
+            Console.WriteLine("Nakon nekoliko dodatih cw na brencu proba3, presli smo na master granu i dodali ovaj tekst");
         }
     }
 }
